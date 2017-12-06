52e99175bbfa61bd390fd01aa064d95d2d559bb0
willdallas <32796325+willdallas@users.noreply.github.com>
2017-12-06 20:04:08 +0000

    fdsf

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,94 @@
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
-    private static ArrayList<ArrayList<String>> personArray = new ArrayList<ArrayList<String>>();
+    private static ArrayList<ArrayList<String>> personArray = new ArrayList<>();
+    private static ArrayList<ArrayList<String>> santasPersonArray = new ArrayList<>(personArray);
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         fillArray();
 
-
-        for (int i = 0; i < personArray.size(); i++) {
+        while (true) {
             System.out.print("\nWho is this!? Santa? ");
-            String Santa = scan.nextLine();
-            santa(Santa);
-        }
+            String input;
+            input = scan.nextLine();
+            if (input.equals("exit")) System.exit(0);
+            santa(input);
 
+        }
 
     }
 
     private static void fillArray() {
-
-
-
-
+        Scanner scanFile = null;
+        try {
+            scanFile = new Scanner(new FileInputStream("data.txt")).useDelimiter(",");
+        } catch (Exception e) {
+            System.out.println("error with file");
+            System.exit(0);
+        }
+        int index = 0;
+        while (scanFile.hasNextLine()) {
+            personArray.add(new ArrayList<>());
+            while (scanFile.hasNext()) {
+                personArray.get(index).add(scanFile.next());
+            }
+            index++;
+        }
     }
 
     private static void santa(String name) {
-        Scanner scan = new Scanner(System.in);
-        if (name.equals("Dhen"))        {
-            System.out.println("hey country girl");
-            System.out.println("just type in who you'd like to give to. Won't affect the programme, I promise ");
-            String wowEstelle = scan.nextLine();
-            System.out.println("ok, unfortunately " + wowEstelle + " is not an option. Let's get to the business");
-            System.out.println("Instead, you will get: ");
-            int randomInt;
+        ArrayList<ArrayList<String>> santasPersonArray = new ArrayList<>(personArray);
 
-            do {
-                randomInt = (int)(Math.random() * personArray.size());
-            }
-            while (personArray.get(randomInt).equals("Dhen"));
-
-            System.out.print(personArray.get(randomInt));
-            personArray.remove(randomInt);
-        }
-        if (name.equals("Estelle"))        {
-            System.out.println("hey country girl");
-            System.out.println("just type in who you'd like to give to. Won't affect the programme, I promise ");
-            String wowEstelle = scan.nextLine();
-            System.out.println("ok, unfortunately " + wowEstelle + " is not an option. Let's get to the business");
-            System.out.println("Instead, you will get: ");
+        if (name.equals("Dhen") || name.equals("dhen")) {
+            System.out.println(santasPersonArray.get(3).get(1));
+            System.out.println(santasPersonArray.get(3).get(1));
             int randomInt;
             do {
-                randomInt = (int)(Math.random() * personArray.size());
+                randomInt = (int) (Math.random() * santasPersonArray.size());
             }
-            while (personArray.get(randomInt).equals("Estelle"));
-            System.out.print(personArray.get(randomInt));
-            personArray.remove(randomInt);
+            while (santasPersonArray.get(randomInt).get(0).equals("Dhen"));
+            System.out.print(santasPersonArray.get(randomInt).get(0));
+            santasPersonArray.remove(randomInt);
         }
-        if (name.equals("Dooho"))        {
-            System.out.println("hey country girl");
-            System.out.println("just type in who you'd like to give to. Won't affect the programme, I promise ");
-            String wowEstelle = scan.nextLine();
-            System.out.println("ok, unfortunately " + wowEstelle + " is not an option. Let's get to the business");
-            System.out.println("Instead, you will get: ");
+        if (name.equals("Estelle")) {
+            System.out.println(santasPersonArray.get(2).get(1));
+            System.out.println(santasPersonArray.get(2).get(1));
             int randomInt;
             do {
-                randomInt = (int)(Math.random() * personArray.size());
+                randomInt = (int) (Math.random() * santasPersonArray.size());
             }
-            while (personArray.get(randomInt).equals("Dooho"));
-            System.out.print(personArray.get(randomInt));
-            personArray.remove(randomInt);
+            while (santasPersonArray.get(randomInt).get(0).equals("Estelle"));
+            System.out.print(santasPersonArray.get(randomInt).get(0));
+            santasPersonArray.remove(randomInt);
         }
-        if (name.equals("Will"))        {
-            System.out.println("hey country girl");
-            System.out.println("just type in who you'd like to give to. Won't affect the programme, I promise ");
-            String wowEstelle = scan.nextLine();
-            System.out.println("ok, unfortunately " + wowEstelle + " is not an option. Let's get to the business");
-            System.out.println("Instead, you will get: ");
+        if (name.equals("Dooho")) {
+            System.out.println(santasPersonArray.get(1).get(1));
+            System.out.println(santasPersonArray.get(1).get(1));
             int randomInt;
             do {
-                randomInt = (int)(Math.random() * personArray.size());
+                randomInt = (int) (Math.random() * santasPersonArray.size());
             }
-            while (personArray.get(randomInt).equals("Will"));
-            System.out.print(personArray.get(randomInt));
-            personArray.remove(randomInt);
+            while (santasPersonArray.get(randomInt).get(0).equals("Dooho"));
+            System.out.print(santasPersonArray.get(randomInt).get(0));
+            santasPersonArray.remove(randomInt);
+        }
+        if (name.equals("Will")) {
+            System.out.println(santasPersonArray.get(0).get(1));
+            System.out.println(santasPersonArray.get(0).get(1));
+            int randomInt;
+            do {
+                randomInt = (int) (Math.random() * santasPersonArray.size());
+            }
+            while (santasPersonArray.get(randomInt).get(0).equals("Will"));
+            System.out.print(santasPersonArray.get(randomInt).get(0));
+            santasPersonArray.remove(randomInt);
         }
     }
 }
